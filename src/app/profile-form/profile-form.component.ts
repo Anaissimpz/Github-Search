8b75f9b462a6ad717c644dc794eef818133d74c3
Anaissimpz <anaissimpenzwe@gmail.com>
2019-02-04 16:53:22 +0200

    add the input

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GitService } from '../gits/git.service';
+import {User} from '../user';
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
