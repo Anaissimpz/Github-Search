f8eb172018e40ec9f24c29ecda19f650ce65d8cf
Anaissimpz <anaissimpenzwe@gmail.com>
2019-02-06 08:41:56 +0200

    import the components to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { GitService } from './gits/git.service';
 
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    ProfileFormComponent
+    ProfileFormComponent,
+    StrikethroughDirective,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [GitService],
   bootstrap: [AppComponent]
