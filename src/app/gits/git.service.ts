160f79b6b2c0d4cb5c60eb2d38575b4e24101415
Anaissimpz <anaissimpenzwe@gmail.com>
2019-02-06 12:17:57 +0200

    final commit

--- src/app/gits/git.service.ts
+++ src/app/gits/git.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../user';
-import {environment} from '../../environments/environment';
 import {Repository} from '../repository';
 
 
@@ -59,9 +58,11 @@ getRepoInfo(username) {
     html_url: string;
     clone_url: string;
 }
-this.http.get<ApiResponse>(environment.apiUrl + username + environment.apiRepokey).subscribe(response => {
+  const promise = new Promise((resolve, reject) => {
+    this.http.get<ApiResponse>("https://api.github.com/users/" + username + '/repos?access_token=8394b5a9f426df57eb97e4b47ade0f1c6d5cda49').subscribe(response => {
   
-    this.items = response;  
+    this.items = response;
   });
+})
 }
 }
