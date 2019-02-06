8cd3dc6df5db60df988606d79368bda73e3dd75f
Anaissimpz <anaissimpenzwe@gmail.com>
2019-02-06 08:37:20 +0200

    add an event on key at profile-form.ts

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { GitService } from '../gits/git.service';
-import {User} from '../user';
+
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
   styleUrls: ['./profile-form.component.css']
 })
 export class ProfileFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  user = {
+    user : ""
   }
 
+onKey(event: any) { // without type info
+  this.user += event.target.value;
 }
+  submitSearch() {
+    this.gitService.getProfileInfo(this.user.user);
+    this.repositoryService.getRepoInfo(this.user.user);
+  }
+   constructor(private gitService: GitService, private repositoryService: GitService) { }
+  ngOnInit() {
+  }
+ }
